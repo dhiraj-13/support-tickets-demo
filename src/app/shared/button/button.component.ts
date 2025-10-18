ae87e3045bcd091e944725d1c54ee6a87368790a
Dhiraj Chhabria <Dhiraj@anils-Mac-mini.local>
2025-10-18 12:26:33 +0530

    vanilla

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   // selector: 'app-button',
-    selector: 'button[appButton]',
+    selector: 'button[appButton], a[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
